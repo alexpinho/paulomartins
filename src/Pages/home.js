acbb6fd82f9086bf253bcdb34fbb54f6f27cbc43
alexpinho <arp2323arp@gmail.com>
2021-11-23 14:52:53 +0000

    hero banner

--- src/Pages/home.js
+++ src/Pages/home.js
@@ -8,6 +8,7 @@ function Home(){
     <Layout>
       <div className="home">
         <div className="hero">
+        <img src="images/banner-1.jpg" className="hero-banner" alt="" />
         <div className="big-title-Book">{t("hero-title-1")} <br/> 
         {t("hero-title-2")} <span className="big-title-BookItalic">{t("hero-title-3")}</span></div>
         </div>

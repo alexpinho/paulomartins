5f194a941153dbff2c97fd4670132f61a077a1ba
alexpinho <arp2323arp@gmail.com>
2021-11-25 18:25:40 +0000

    home projects + files names correction

--- src/Pages/Components/layout.js
+++ src/Pages/Components/layout.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Navbar from './navbar';
+import Navbar from './Navbar';
 
 const Layout = ({ children }) => {
   return (
--- src/Pages/home.js
+++ src/Pages/home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Layout from './Components/layout';
+import Layout from './Components/Layout';
 import { useTranslation } from "react-i18next";
 
 function Home(){
@@ -9,16 +9,40 @@ function Home(){
       <div className="home">
 
         <div className="home-hero">
-        <img src="images/banner-1.jpg" className="home-hero-banner" alt="" />
-        <div className="big-title-Book">{t("hero-title-1")} <br/> 
-        {t("hero-title-2")} <span className="big-title-BookItalic">{t("hero-title-3")}</span></div>
+            <img src="images/banner-1.jpg" className="home-hero-banner" alt="" />
+            <div className="big-title-Book">{t("hero-title-1")} <br/> 
+            {t("hero-title-2")} <span className="big-title-BookItalic">{t("hero-title-3")}</span>
+            </div>
         </div>
 
         <div className="home-portfolio">
-        <div className="title-Regular">{t("home-portfolio-title")}</div>
-        <div className="home-portfolio-note">
-        <div className="text-note">{t("home-portfolio-text-note")}</div>
-        </div>
+            <div className="home-portfolio-intro">
+                <div className="home-portfolio-intro-left">
+                    <div className="text-detail">2011-2021</div>
+                </div>
+                <div className="title-Regular">{t("home-portfolio-title")}</div>
+                <div className="home-portfolio-intro-right">
+                    <a href='/portfolio' className="text-detail">{t("home-portfolio-topage")}</a>
+                </div>
+            </div>
+                <div className="home-portfolio-note">
+                    <div className="text-note">{t("home-portfolio-text-note")}</div>
+                </div>
+                <div className="home-portfolio-1">
+                    <img src="images/homeportfolio-1.png" className="home-portfolio-1-img" alt="" />
+                    <div className="home-portfolio-1-titles">
+                        <div className="home-subtitle">{t("home-portfolio-home1")}</div>
+                        <div className="home-subtitle-location">{t("home-portfolio-home1-location")}</div>
+                    </div>
+                </div>
+                <div className="home-portfolio-2">
+                    <img src="images/homeportfolio-2.jpg" className="home-portfolio-2-img" alt="" />
+                    <img src="images/homeportfolio-3.jpg" className="home-portfolio-3-img" alt="" />
+                    <div className="home-portfolio-2-titles">
+                        <div className="home-subtitle">{t("home-portfolio-home1")}</div>
+                        <div className="home-subtitle-location">{t("home-portfolio-home1-location")}</div>
+                    </div>
+                </div>
         </div>
         
       </div>

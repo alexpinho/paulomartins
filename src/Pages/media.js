1f746b3aed0736ed0dab906ac5ba7f85e5833916
Alexandre Lucas <alexandrelucas@Alexandres-MacBook-Pro.local>
2021-12-05 12:13:10 +0000

    media-hero completed

--- src/Pages/media.js
+++ src/Pages/media.js
@@ -4,9 +4,27 @@ import Layout from './Components/layout';
 const Media = () => {
   return (
     <Layout>
-      <div className="Media">
-        <h1>Media.</h1>
+      <div className="media">
+        <div className="media-hero">
+          <div className="row">
+            <div className="big-title-Book">Media </div>
+            <div className="col-xs-offset-3 col-xs-5">
+            <div className="text-note">Selection of news, references about the studio in online and printed publications, as well as awards and comments to our projects.</div>
+          </div>
+          </div>
+        </div>
       </div>
+
+              {/* FALTA OS BOTOES COM OS FILTROS PARA ESCOLHER A ORDEM DOS PROJETOS*/}
+<div className="media-publication">
+
+
+
+
+
+</div>
+
+
     </Layout>
   );
 };

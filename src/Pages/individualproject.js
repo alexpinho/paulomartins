4be63b0115c5edabb3cf071c078cd8b4ddb6e079
Alexandre Lucas <alexandrelucas@Alexandres-MacBook-Pro.local>
2021-12-07 02:29:25 +0000

    images and information added

--- src/Pages/individualproject.js
+++ src/Pages/individualproject.js
@@ -5,8 +5,15 @@ const individualproject = () => {
     return (
       <Layout>
         <div className="individualproject">
-          <h1>Contacts.</h1>
+            <div className="individualproject-hero">
+                <div className="text-detail">2020</div> 
+                <div className="big-title-Book col-xs-4">Pisco House </div>       
+                <img src="images/picohousesmall.jpg" classeName="individualproject-hero-banner col-xs-6" alt=""/>
+                <div className="text-note col-xs-2"><b>client</b> Confidential <br/> <b>place</b> Águeda . Portugal <br/><b>gross</b> area 550 sqm <br/> <b>status</b> design development</div>
+           </div>
+           <img src="images/picohousebig.jpg" className="col-xs-12" alt=""/>
         </div>
+      
       </Layout>
     );
   };

16fd294cb63cc5b7e0bab94d907a731608dd3479
ricardosacadura <ricardorochasacadura@gmail.com>
2022-01-18 16:07:42 +0000

    jobs animate 2.0

--- src/Pages/Components/CardJobsPopup.js
+++ src/Pages/Components/CardJobsPopup.js
@@ -4,8 +4,7 @@ import {motion} from "framer-motion";
 
 function CardJobsPopup(props) {
     return (props.trigger) ? (
-        <motion.div className='popup' initial={{opacity:0, scale:0.7,borderRadius:0}} animate={{opacity:["0%", "50%", "100%"], scale:1,
-            borderRadius: ["30%", "30%", "0%"]}} transition={{duration: 0.4}}>
+        <motion.div className='popup' initial={{opacity:0}} animate={{opacity:["0%", "50%", "100%"]}} transition={{duration: 1}}>
             <div className='popup-inner text-note'>
                 <button className='close-btn' onClick={() => props.setTrigger(false)}>x</button>
                 {props.children}
